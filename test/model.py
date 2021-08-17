937b96a4d43d1169776374e29f4d8e07b20afe59
Saima Khan <tithi888khan@gmail.com>
2021-08-17 08:21:49 +0600

    Fixes modeling issues

--- test/model.py
+++ test/model.py
@@ -1,27 +1,10 @@
-# from numpy.lib.npyio import load
-# import numpy as np
-# from sklearn.linear_model import SGDRegressor
-# from sklearn.datasets import load_iris
-
-# from regresspy import Regression
-# from regresspy import rmse
-
-
-# iris = load_iris()
-# # We will use sepal length to predict sepal width
-# X = iris.data[:, 0].reshape(-1, 1)
-# Y = iris.data[:, 1].reshape(-1, 1)
-
-# #TODO Perform a linear regression using sklearn and calculate training rmse.
-# # Use the SGDRegressor and only select set learning rate and epochs.
-
-
-# #TODO Perform a linear regression using your code and calculate training rmse.
-
 from numpy.lib.npyio import load
 import numpy as np
+import math
 from sklearn.linear_model import SGDRegressor
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 from regresspy.regression import Regression
 from regresspy.loss import rmse
@@ -42,7 +25,7 @@ model = SGDRegressor()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
-mse = mean_squard_error(y_test_, y_pred)
+mse = mean_squared_error(y_test, y_pred)
 prev_rmse = math. sqrt(mse)
 print("Previous model " + str(prev_rmse))
 
@@ -55,5 +38,5 @@ print(my_model._weights['W'].shape)
 my_model_y_pred = my_model.predict(X_test)
 print(y_test.shape)
 print(my_model_y_pred.shape)
-my_model_rmse = rmse(y_test_,my_model_y_pred)
+my_model_rmse = rmse(y_test,my_model_y_pred)
 print(my_model_rmse)
